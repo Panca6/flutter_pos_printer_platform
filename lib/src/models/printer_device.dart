7768c66b135df9328ac78d8af3954108f6b73303
panca6 <pancasolusibisnis6@gmail.com>
2024-10-02 22:47:31 +0700

    update dependencies

--- lib/src/models/printer_device.dart
+++ lib/src/models/printer_device.dart
@@ -1,11 +1,42 @@
+import 'dart:convert';
 import 'dart:io';
 
 class PrinterDevice {
   String name;
-  String operatingSystem = Platform.operatingSystem;
+  String? operatingSystem = Platform.operatingSystem;
   String? vendorId;
   String? productId;
   String? address;
 
-  PrinterDevice({required this.name, this.address, this.vendorId, this.productId});
+  PrinterDevice({
+    required this.name,
+    this.address,
+    this.vendorId,
+    this.productId,
+    this.operatingSystem,
+  });
+
+  Map<String, dynamic> toMap() => {
+        'name': name,
+        'operatingSystem': operatingSystem,
+        'vendorId': vendorId,
+        'pr8oductId': productId,
+        'address': address,
+      };
+
+  factory PrinterDevice.fromMap(Map<String, dynamic> json) {
+    return PrinterDevice(
+      name: json['name'],
+      operatingSystem: json['operatingSystem'],
+      vendorId: json['vendorId'],
+      productId: json['productId'],
+      address: json['address'],
+    );
+  }
+
+  factory PrinterDevice.fromJson(String json) => PrinterDevice.fromMap(
+        jsonDecode(json),
+      );
+
+  String toJson() => jsonEncode(toMap());
 }
